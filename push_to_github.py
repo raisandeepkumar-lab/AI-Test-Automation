2b1e3c5413a690e0332e07f745429a79ea47598a
SandeepRai <sandeepkumarrai@hotmail.com>
2026-05-01 16:56:31 +0530

    Removing Testcase for full code testing

--- push_to_github.py
+++ push_to_github.py
@@ -5,14 +5,12 @@ def push_to_github(filename):
     g = Github(os.getenv("GITHUB_TOKEN"))
     repo = g.get_repo(os.getenv("GITHUB_REPO"))
 
-    with open(filename, "r") as file:
+    with open(f"./tests/{filename}", "r") as file:
         content = file.read()
 
     repo.create_file(
         path=f"tests/{filename}",
         message=f"Add test case {filename}",
         content=content,
-        branch="main"
+        branch=os.getenv("branch")
     )
-
-push_to_github("test_scrum-1.py")
--- save_the_test_case.py
+++ save_the_test_case.py
@@ -1,7 +1,7 @@
 def save_test_file(issue_key, test_code):
     filename = f"test_{issue_key.lower()}.py"
 
-    with open(filename, "w") as f:
+    with open(f"./tests/{filename}", "w") as f:
         f.write(test_code)
 
     return filename
